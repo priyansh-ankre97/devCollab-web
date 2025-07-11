67dd76a143557c8a2532f95d66738b7d72da77c7
priyansh ankre <priyansh@reachmobile.com>
2025-07-11 18:20:18 +0530

    init project  and added router

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Body from "./Body";
+import Login from "./Login";
+import Signup from "./Signup";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Login.jsx
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import { addUser } from "./utils/userSlice";
+import { useDispatch } from "react-redux";
+
+const Login = () => {
+  const [emailId, setEmailId] = useState("Priyansh@gmail.com");
+  const [password, setPassword] = useState("Priyansh@123");
+  const dispatch = useDispatch();
+  const handleLogin = async () => {
+    try {
+      const res = await fetch("http://localhost:7777/login", {
+        method: "POST",
+        body: JSON.stringify({
+          emailId,
+          password,
+        }),
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const data = await res.json();
+      dispatch(addUser(data));
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  return (
+    <div className="flex justify-center my-10">
+      <div className="card card-dash bg-base-300 w-96">
+        <div className="card-body">
+          <h2 className="card-title">Login!</h2>
+          <fieldset className="fieldset">
+            <legend className="fieldset-legend">Enter you emailId</legend>
+            <input
+              type="text"
+              className="input"
+              placeholder="Type EmailId here"
+              value={emailId}
+              onChange={(e) => setEmailId(e.target.value)}
+            />
+            {/* <p className="label">Optional</p> */}
+          </fieldset>
+          <fieldset className="fieldset">
+            <legend className="fieldset-legend">Enter you Password</legend>
+            <input
+              type="text"
+              className="input"
+              placeholder="Type Password here"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            {/* <p className="label">Optional</p> */}
+          </fieldset>
+          <div className="card-actions justify-center">
+            <button className="btn btn-primary" onClick={handleLogin}>
+              login
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;

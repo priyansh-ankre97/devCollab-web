3b9658eb950e4ed93c56c2dd7691c823a1b62390
priyansh ankre <priyansh@reachmobile.com>
2025-07-15 14:57:10 +0530

    feat: logout

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,28 @@
 import React from "react";
-import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
 import ROUTES from "../utils/routingUrls";
+import { BASE_URL } from "../utils/constant";
+import { removeUser } from "../utils/userSlice";
 
 const NavBar = () => {
   const userInfo = useSelector((store) => store.user);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const handleLogout = async () => {
+    try {
+      const response = await fetch(BASE_URL + "/logout", {
+        method: "POST",
+        credentials: "include",
+      });
+      if (response.ok) {
+        dispatch(removeUser());
+        navigate(ROUTES.LOGIN);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div className="navbar bg-base-300 shadow-sm">
       <div className="flex-1">
@@ -37,7 +55,7 @@ const NavBar = () => {
                 <a>Settings</a>
               </li>
               <li>
-                <a>Logout</a>
+                <a onClick={handleLogout}>Logout</a>
               </li>
             </ul>
           </div>
